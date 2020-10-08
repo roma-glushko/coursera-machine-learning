7f3cf4056f662ae55fa5e139861b2821e93a08cc
Roman Glushko <roman.glushko.m@gmail.com>
2020-10-08 12:54:49 +0300

    📦 Added week 9 about anomaly detection and recommender system

--- 2-linear-regression/lab/computeCost.m
+++ 2-linear-regression/lab/computeCost.m
@@ -19,7 +19,7 @@ endfor
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-J = 1/(2*m) * sum ;
+J = 1/(2*m) * sum;
 
 % =========================================================================
 
